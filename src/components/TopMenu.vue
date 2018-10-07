21919f80d5f2bff184e3aa6b0a632f636b900972
Artemov Nikita <neki712@yandex.ru>
2018-10-07 16:52:42 +0300

    add basket to top menu

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,23 +1,36 @@
 <template>
     <div class="nav">
         <router-link to="/">Mainpage</router-link>
+        <router-link to="/basket">
+            <basket />
+        </router-link>
     </div>
 </template>
 
 <script>
+import Basket from '@/components/Basket';
+
 export default {
   name: 'TopMenu',
+  components: {
+    Basket,
+  },
 };
 </script>
 
 <style scoped>
 .nav {
-    padding: 30px;
+    padding: 15px;
+    display: flex;
 }
 
 .nav a {
+    border: 1px solid silver;
     font-weight: bold;
     color: #2c3e50;
+    margin: 15px;
+    padding: 15px;
+    text-decoration: none;
 }
 
 .nav a.router-link-exact-active {
