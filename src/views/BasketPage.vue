c2b4978df4cdc029b107a7b98988914fe2c0b800
Artemov Nikita <neki712@yandex.ru>
2018-10-07 17:25:28 +0300

    add items list on basket page

--- src/views/BasketPage.vue
+++ src/views/BasketPage.vue
@@ -1,10 +1,26 @@
 <template>
-    <div></div>
+    <div class="page basket-page">
+        <items-list :items="items">
+            <template slot="empty">
+                <h1>Your basket is empty.</h1>
+            </template>
+        </items-list>
+    </div>
 </template>
 
 <script>
+import ItemsList from '@/components/ItemsList';
+
 export default {
   name: 'BasketPage',
+  components: {
+    ItemsList,
+  },
+  computed: {
+    items() {
+      return this.$store.get('itemsData', this.$store.get('basket/items'));
+    },
+  },
 };
 </script>
 
